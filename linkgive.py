647a206558a382ce8a3e4e97bdb84fd7941107fa
GaksP0wer <mehmetkayrabul@gmail.com>
2024-06-18 15:09:53 +0300

    Add files via upload

---
+++ linkgive.py
@@ -0,0 +1,114 @@
+import os
+import time
+import sys
+def animated(text):
+  for x in text:
+    sys.stdout.write(x)
+    sys.stdout.flush()
+    time.sleep(0.006)
+    
+
+logo = '''
+
+            \033[1;33m ___ _              
+            / _ \__ _| | __ _ __ __ _
+           / /_\/ _` | |/ / '__/ _` |
+          / /_\\ (_| | <| | | (_| |
+          \____/\__,_|_|\_\_| \__,_|
+                           
+             __ _        
+            / / ___ __ _ _()_ __   
+           / / / _ \ / _` | | '_ \  
+          / /__| (_) | (_|| | | | |
+          \____/\___/ \__,|_|_| |_|
+                     |___/          
+
+'''
+
+animated(logo)
+
+şifre = "Gakra120323"
+
+
+givenŞifre = input("\033[0;32mŞifreyi Gir Öşşek: ")
+if givenŞifre == şifre:
+ print("Şifre Onaylandı")
+  
+ logo2 = '''
+ 
+  _     ___ _   _ _  ___     _____ ____  _____ 
+ | |   |_ _| \ | | |/ / |   | ____|  _ \| ____|
+ | |    | ||  \| | ' /| |   |  _| | |_) |  _|  
+ | |___ | || |\  | . \| |___| |___|  _ <| |___ 
+ |_____|___|_|_\_|_|\_\_____|_____|_|_\_\_____|
+  / ___| ____| |   |  _ \_ _| \ | |_ _|__  /   
+ | |  _|  _| | |   | | | | ||  \| || |  / /    
+ | |_| | |___| |___| |_| | || |\  || | / /_    
+  \____|_____|_____|____/___|_| \_|___/____|   
+                                               
+
+ '''
+ 
+ animated(logo2)
+ 
+ Anan = '''
+\033[0;34mBedava Admin P4nel Link:https://tavsancik.online
+ '''
+ 
+ Anan2 = '''
+             \033[0;35mP4nel'e VPN Açarak Gir
+                      |
+                      |
+ '''
+ 
+ 
+
+ Anan4 = '''
+        \033[0;32mCamPhish Tüm Kodları İndirme Linki
+                        |
+                        |
+ ''' 
+ 
+ 
+ 
+ Anan3 = '''
+        \033[1;33mhttps://www.mediafire.com/file/w3vw7glpfqnf183/CamPhish.txt/file
+ '''
+ 
+ 
+ Anan31 = '''
+          \033[0;34mAmerikanın Sızdırılmış Güvenlik Kameraları
+                       |
+                       |
+ '''
+ 
+ Anan62 = '''
+         \033[0;34mhttp://77.173.146.165:8080/multi.html
+ '''
+ 
+ 
+ 
+ 
+ animated(Anan2)
+ animated(Anan)
+ animated(Anan4)
+ animated(Anan3)
+ animated(Anan31)
+ animated(Anan62)
+ 
+else:
+  print("              \033[0;31mHadi Köyüne Öşşşek")
+  
+  logo4 = '''
+  
+                 \033[0;31m_____  _____ 
+                / ____|/ ____|
+               | (___ | |  __ 
+                \___ \| | |_ |
+                ____) | |__| |
+               |_____/ \_____|
+                
+                
+  '''
+  
+  animated(logo4)
